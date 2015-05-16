302aa8a3af0447a6920884bde7c851d05c7caefa
Quinn Hannah-White <quinnfortehwin@gmail.com>
2015-05-16 13:17:01 -0600

    merging conflicts

--- src/edu/du/cs/quinn/Music/Key.java
+++ src/edu/du/cs/quinn/Music/Key.java
@@ -3,19 +3,14 @@ package edu.du.cs.quinn.Music;
 
 public class Key {
 	private static Key instance = null;
-	@SuppressWarnings("unused")
 	private int thePitch;
-	private int minorRaiseValue;
+	private int[] scalePitches;
+	private boolean isMajor;
 	private Key(int p, boolean isMajor) {
 		thePitch = p;
-		if(isMajor)
-		{
-			minorRaiseValue = 0;
-		}
-		else
-		{
-			minorRaiseValue = -1;
-		}
+		this.isMajor = isMajor;
+		scalePitches = new int[7];
+		setScalePitches();
 	}
 	
 	public static Key getInstance(int p, boolean isMajor) {
@@ -25,9 +20,34 @@ public class Key {
 	   return instance;
 	}
 	
+	public int getScalePitch(int degree)
+	{
+		return scalePitches[degree - 1];
+	}
+	
+	private void setScalePitches()
+	{
+		scalePitches[0] = thePitch;
+		scalePitches[1] = thePitch + 2;
+		scalePitches[3] = thePitch + 5;
+		scalePitches[4] = thePitch + 7;
+		if (isMajor)
+		{
+			scalePitches[2] = thePitch + 4;
+			scalePitches[5] = thePitch + 9;
+			scalePitches[6] = thePitch + 11;
+		}
+		else
+		{
+			scalePitches[2] = thePitch + 3;
+			scalePitches[5] = thePitch + 8;
+			scalePitches[6] = thePitch + 10;
+		}
+	}
+	
 	public boolean isMajor()
 	{
-		return minorRaiseValue == 0;
+		return isMajor;
 	}
 	
 }
